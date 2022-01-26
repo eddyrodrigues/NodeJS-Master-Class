114f7ffa5a7af03632395c15fca0c3d2fee38ad8
Eduardo Oliveira Rodrigues <edu.ol.ro@gmail.com>
2022-01-26 00:04:45 -0300

    Update loop.js
    adding labelled loops

--- loop.js
+++ loop.js
@@ -6,3 +6,21 @@ for (iterator=0; iterator < items.length ; iterator++)
 {
   console.log(items[iterator]);
 }
+
+
+// breaking laballed loopings:
+
+const items = ["item_1", "banana", "orange", "apple"];
+
+outerLoop:
+for (iterator=0; iterator < items.length ; iterator++)
+{
+  innerLoop:
+  for (iterator2 = 0; iterator2 < items.length ; iterator2++)
+  {
+    console.log(items[iterator2]);
+    if (items[iterator] == "orange") break outerLoop;
+  }
+}
+
+
