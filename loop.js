19810cf802fbbf8dbac5b4a39a3f61f63afc1d26
Eduardo Oliveira Rodrigues <edu.ol.ro@gmail.com>
2022-01-26 00:00:19 -0300

    Create loop.js
    for loop

---
+++ loop.js
@@ -0,0 +1,8 @@
+// applying loopp throw items
+
+const items = ["item_1", "banana", "orange", "apple"];
+
+for (iterator=0; iterator < items.length ; iterator++)
+{
+  console.log(items[iterator]);
+}
