07cd0a3103c4e85c288deb09af7452a81ec09d57
Eduardo Oliveira Rodrigues <edu.ol.ro@gmail.com>
2022-01-26 00:23:48 -0300

    Update loop.js

--- loop.js
+++ loop.js
@@ -23,4 +23,13 @@ for (iterator=0; iterator < items.length ; iterator++)
   }
 }
 
+// for of loop
 
+const meta_data = [{1:"nome", 2: "sobrenome" }, {1:"nome2", 2: "sobrenome2" }];
+
+for (const meta_data_item of meta_data) {
+  console.log(meta_data_item);
+}
+// console log result: 
+// {1: 'nome', 2: 'sobrenome'}
+// {1: 'nome2', 2: 'sobrenome2'}
